13366b8a1167f9fecc6e690758f3e6cc453be8ea
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-04-14 19:22:38 -0300

    server/refactor: inclui try-catch nos 'controller's

--- amazon-server/src/controllers/product.controller.ts
+++ amazon-server/src/controllers/product.controller.ts
@@ -2,21 +2,94 @@ import type { Request, Response } from "express";
 import { Product, Seller } from "../models";
 
 export const createProduct = async (req: Request, res: Response) => {
-  const data = await Product.create(req.body)
-  res.json(data)
+  try {
+    const newProduct = await Product.create(req.body)
+    res.json(newProduct)
+      .sendStatus(201)
+      .send("Produto criado com sucesso")
+
+    if (!newProduct || newProduct == null) {
+      res.status(422).send("Valores inválidos ou campos incompletos")
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erro ao criar Produto"
+    })
+  }
 }
 
 export const getProducts = async (_: Request, res: Response) => {
-  const data = await Product.findAll({ include: Seller })
-  res.json(data)
+  try {
+    const data = await Product.findAll({ include: Seller })
+    res.json(data).sendStatus(200)
+
+    if (!data) {
+      return res.status(404).json({
+        error: "Produto não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erro ao buscar Produto"
+    })
+  }
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const productData = await Product.findOne({ where: { id: req.params.id } })
+    res.json(productData).sendStatus(200)
+
+    if (!productData) {
+      return res.status(404).json({
+        error: "Produto não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: `Erro ao buscar Produto com id ${req.params.id}`
+    })
+  }
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
-  await Product.update(req.body, { where: { id: req.params.id } })
-  res.sendStatus(204)
+  try {
+    const productData = await Product.update(req.body, { where: { id: req.params.id } })
+    res.json(productData)
+      .sendStatus(204)
+      .send("Produto atualizado com sucesso")
+
+    if (!productData) {
+      return res.status(404).json({
+        error: "Produto não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: `Erro ao atualizar produto ${req.params.id}`
+    })
+  }
 }
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  await Product.destroy({ where: { id: req.params.id } })
-  res.sendStatus(204)
+  try {
+    const productData = await Product.destroy({ where: { id: req.params.id } })
+    res.sendStatus(200)
+      .send("Produto deletado com sucesso")
+
+    if (!productData) {
+      return res.status(404).json({
+        error: "Produto não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erro ao buscar Produto"
+    })
+  }
 }
--- amazon-server/src/controllers/seller.controller.ts
+++ amazon-server/src/controllers/seller.controller.ts
@@ -2,21 +2,94 @@ import type { Request, Response } from "express";
 import { Seller } from "../models";
 
 export const createSeller = async (req: Request, res: Response) => {
-  const data = await Seller.create(req.body)
-  res.json(data)
+  try {
+    const newSeller = await Seller.create(req.body)
+    res.json(newSeller)
+      .sendStatus(201)
+      .send("Vendedor criado com sucesso")
+
+    if (!newSeller || newSeller == null) {
+      res.status(422).send("Valores inválidos ou campos incompletos")
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erro ao criar Vendedor"
+    })
+  }
 }
 
 export const getSellers = async (_: Request, res: Response) => {
-  const data = await Seller.findAll()
-  res.json(data)
+  try {
+    const data = await Seller.findAll()
+    res.json(data).sendStatus(200)
+
+    if (!data) {
+      return res.status(404).json({
+        error: "Vendedor não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Erro ao criar Vendedor"
+    })
+  }
+}
+
+export const getSellerById = async (req: Request, res: Response) => {
+  try {
+    const sellerData = await Seller.findOne({ where: { id: req.params.id } })
+    res.json(sellerData).sendStatus(200)
+
+    if (!sellerData) {
+      return res.status(404).json({
+        error: "Vendedor não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: `Erro ao buscar Vendedor com id ${req.params.id}`
+    })
+  }
 }
 
 export const updateSeller = async (req: Request, res: Response) => {
-  await Seller.update(req.body, { where: { id: req.params.id } })
-  res.sendStatus(204)
+  try {
+    const sellerData = await Seller.update(req.body, { where: { id: req.params.id } })
+    res.json(sellerData)
+      .sendStatus(204)
+      .send("Vendedor atualizado com sucesso")
+
+    if (!sellerData) {
+      return res.status(404).json({
+        error: "Vendedor não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: `Erro ao atualizar vendedor com id ${req.params.id}`
+    })
+  }
 }
 
 export const deleteSeller = async (req: Request, res: Response) => {
-  await Seller.destroy({ where: { id: req.params.id } })
-  res.sendStatus(204)
+  try {
+    const sellerData = await Seller.destroy({ where: { id: req.params.id } })
+    res.sendStatus(200)
+      .send("Vendedor deletado com sucesso")
+
+    if (!sellerData) {
+      return res.status(404).json({
+        error: "Vendedor não encontrado"
+      })
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      error: `Erro ao buscar Vendedor ${req.params.id}`
+    })
+  }
 }
